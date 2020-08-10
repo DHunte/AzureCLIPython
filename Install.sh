2acc4519735448c152d0307f88f9b60ac34d23fc
DH <46441813+DHunte@users.noreply.github.com>
2020-08-10 23:39:30 +0100

    Added upgrade for pip
    Some setups require pip to be upgraded if no prior update has been performed.

--- Install.sh
+++ Install.sh
@@ -1,5 +1,6 @@
 sudo apt-get install python3-venv
 sudo apt-get install python3-dev
+sudo pip3 install --upgrade pip
 python3 -m venv env
 . env/bin/activate
 pip3 install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge --upgrade-strategy=eager
